e8f5c4ced82c50f9a6ea29b398564a3018d682cc
Aris Zagakos <aris.zagakos@theblueground.com>
2022-09-12 22:01:00 +0300

    Update cypress config
    Updated cypress config to search for spec files

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://google.com",
+    watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
   },
 });
