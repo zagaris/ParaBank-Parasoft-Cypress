8a86ad695bc7066e6a5e263d0c7b9c8e19cf549d
Aris Zagakos <aris.zagakos@theblueground.com>
2022-09-14 02:54:07 +0300

    Add cypress allure plugin

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,23 @@
 const { defineConfig } = require("cypress");
+import AllureWriter from "@shelex/cypress-allure-plugin/writer";
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://google.com",
     watchForFileChanges: false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      reportDir: "cypress/reports/html",
+      charts: true,
+      reportPageTitle: "My Test Suite",
+      embeddedScreenshots: true,
+      inlineAssets: true,
+    },
+    video: true,
+    setupNodeEvents(on, config) {
+      AllureWriter(on, config);
+      return config;
+    },
   },
 });
